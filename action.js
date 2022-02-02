1d9ba0f208f956168dd53a38b434a1880961f09e
ArdraHaridas <86566382+ArdraHaridas@users.noreply.github.com>
2022-02-02 22:09:46 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,16 @@
+function changeToRed() {
+    document.body.style.backgroundColor = "red";
+    document.getElementById("title").innerHTML="red";
+}
+function changeToOrange() {
+    document.body.style.backgroundColor = "orange";
+    document.getElementById("title1").innerHTML="orange";
+}
+function changeToBlue() {
+    document.body.style.backgroundColor = "blue";
+    document.getElementById("title2").innerHTML="blue";
+}
+function changeToBlack() {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("title3").innerHTML="black";
+}
